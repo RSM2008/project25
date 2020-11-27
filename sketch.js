2f2767a33425f3d57c3e0907714f0dfd4d7d6bbc
RSM2008 <72798943+RSM2008@users.noreply.github.com>
2020-11-27 21:40:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,104 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+
+var dustbinObject;
+var groundObject;
+var paperObject;
+var world;
+
+
+
+
+function setup() {
+	createCanvas(1600,700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	//Create a Ground
+    paperObject=new paper(200,450,70);
+	dustbinObj=new dustbin(1200,650);
+	groundObject=new ground(width/2,670,width,20);
+	//Create a Ground
+
+	var render = Render.create({
+		element: document.body,
+		engine: engine,
+		options: {
+		  width: 1200,
+		  height: 700,
+		  wireframes: false
+		}
+	  });
+
+
+	
+ 	
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+
+  Engine.update(engine);
+  paperObject.display();
+  groundObject.display();
+  dustbinObject.display();
+  
+  
+  
+
+
+  
+
+
+
+
+
+
+
+
+  
+  
+  drawSprites();
+ 
+}
+
+
+function keyPressed(){
+      if(keyCode ===UP_ARROW) {
+
+		Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+	  }
+
+
+
+
+}
